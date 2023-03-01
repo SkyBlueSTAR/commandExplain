7f2be68754074674ec63d28819a211fcfe093fe0
SkyBlueSTAR <111492128+SkyBlueSTAR@users.noreply.github.com>
2023-03-01 16:06:31 +0900

    関数3つ追加 (詳細はnbt_data_io.js)

--- model/file/nbt_data_io.js
+++ model/file/nbt_data_io.js
@@ -1,10 +1,45 @@
 import pnbt from './pnbt.js';
-//nbtToJson(reader:FileReader)
+
+//nbtToJSON(reader:FileReader): Promise<String>
 //This function can throw error.
-export async function nbtToJson(reader){
+export async function nbtToJSON(reader){
     try{
-        const {parsed} = await nbt.parse(Buffer.from(reader.result));
+        const {parsed} = await pnbt.parse(Buffer.from(reader.result),'little');
         return JSON.stringify(parsed, null, 2);
+    }catch(e){
+        return e;
+    }
+}
+
+//JSONToNbt(json:String)
+//Returns a buffer with a serialized nbt value.
+//This function can throw error.
+export async function JSONToNbt(json){
+    try{
+        const obj = JSON.parse(json);
+        return await pnbt.writeUncompressed(obj,'little')
+    }catch(e){
+        return e;
+    }
+}
+
+//nbtToJSON(reader:FileReader): Promise<Object>
+//This function can throw error.
+export async function nbtToObject(reader){
+    try{
+        const {parsed} = await pnbt.parse(Buffer.from(reader.result),'little');
+        return parsed;
+    }catch(e){
+        return e;
+    }
+}
+
+//objectToNbt(obj:Object)
+//Returns a buffer with a serialized nbt value.
+//This function can throw error.
+export async function objectToNbt(obj){
+    try{
+        return await pnbt.writeUncompressed(obj,'little')
     }catch(e){
         return e;
     }
