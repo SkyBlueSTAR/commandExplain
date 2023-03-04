db83729ac10375ae13bdd1fdd62d85268c81acce
Number-Block <126405195+Number-Block@users.noreply.github.com>
2023-03-05 00:29:15 +0900

    データ取得関数に引数indexを追加しました

--- model/json/get_block_data.js
+++ model/json/get_block_data.js
@@ -2,46 +2,46 @@ exports.getBlockIndices = function (structureData) {
     return structureData.block_indices.value.value[0].value;
 }
 
-exports.getBlockName = function (structureData) {
-    return structureData.palette.value.default.value.block_palette.value.value[0].name.value;
+exports.getBlockName = function (structureData, index) {
+    return structureData.palette.value.default.value.block_palette.value.value[index].name.value;
 }
 
-exports.getBlockConditionalBit = function (structureData) {
-    return structureData.palette.value.default.value.block_palette.value.value[0].states.value.conditional_bit.value;
+exports.getBlockConditionalBit = function (structureData, index) {
+    return structureData.palette.value.default.value.block_palette.value.value[index].states.value.conditional_bit.value;
 }
 
-exports.getBlockFacingDirection = function (structureData) {
-    return structureData.palette.value.default.value.block_palette.value.value[0].states.value.facing_direction.value;
+exports.getBlockFacingDirection = function (structureData, index) {
+    return structureData.palette.value.default.value.block_palette.value.value[index].states.value.facing_direction.value;
 }
 
-exports.getBlockCommand = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.Command.value;
+exports.getBlockCommand = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.Command.value;
 }
 
-exports.getBlockCustomName = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.CustomName.value;
+exports.getBlockCustomName = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.CustomName.value;
 }
 
-exports.getBlockExecuteOnFirstTick = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.ExecuteOnFirstTick.value;
+exports.getBlockExecuteOnFirstTick = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.ExecuteOnFirstTick.value;
 }
 
-exports.getBlockLPCommandMode = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.LPCommandMode.value;
+exports.getBlockLPCommandMode = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.LPCommandMode.value;
 }
 
-exports.getBlockLPCondionalMode = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.LPCondionalMode.value;
+exports.getBlockLPCondionalMode = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.LPCondionalMode.value;
 }
 
-exports.getBlockLPRedstoneMode = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.LPRedstoneMode.value;
+exports.getBlockLPRedstoneMode = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.LPRedstoneMode.value;
 }
 
-exports.getBlockTickDelay = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.TickDelay.value;
+exports.getBlockTickDelay = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.TickDelay.value;
 }
 
-exports.getBlockconditionalMode = function (structureData) {
-    return structureData.palette.value.default.value.block_position_data.value[0].value.block_entity_data.value.conditionalMode.value;
+exports.getBlockconditionalMode = function (structureData, index) {
+    return structureData.palette.value.default.value.block_position_data.value[index].value.block_entity_data.value.conditionalMode.value;
 }
--- model/json/get_structure_data.js
+++ model/json/get_structure_data.js
@@ -1,11 +1,7 @@
-//getStructureSize(json:String)<{}>
-//returns the structure size.
 exports.getStructureSize = function (json) {
     return json.value.size.value.value;
 }
 
-//getStructureSize(json:String)<{}>
-//returns the structure data.
 exports.getStructureData = function (json) {
     return json.value.structure.value;
 }
